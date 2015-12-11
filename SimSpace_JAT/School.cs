4d7377ecac195e2d447c9ebaf23870c6c9c0a58a
Symbioting <lordterethias@gmail.com>
2015-12-11 10:02:53 -0500

    Here, in my garage. Knawledge.

--- SimSpace_JAT/School.cs
+++ SimSpace_JAT/School.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the School class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class School : Facility
+    class School : EssentialServicesFacility
     {
+        /// <summary>
+        /// Cost of Schools
+        /// </summary>
+        public const int COST = 500000000;
+
+        /// <summary>
+        /// Creates a school with the assigned values
+        /// </summary>
+        public School()
+            : base()
+        {
+            Maintenance = 5000000;
+            Power = -15;
+        }
     }
 }
