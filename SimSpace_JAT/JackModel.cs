70fcfc58d20ae053928a07f47cd01fdef0f02e26
Symbioting <lordterethias@gmail.com>
2015-12-11 10:08:12 -0500

    Models :D

--- SimSpace_JAT/JackModel.cs
+++ SimSpace_JAT/JackModel.cs
@@ -1,70 +1,296 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the class where Jack's subprograms are created, which are called by the model wrapper after the form(s) call the model wrapper
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.IO;
+using System.Windows.Forms;
 
 namespace SimSpace_JAT
 {
     class JackModel
     {
-        private SharedVariables _variables;
-        public JackModel(PlanetModelWrapper p, SharedVariables v) {
-            _variables = v;
-        }
-        public bool BuildEmergencyServices(int a, int s)
+        // Link to the other programmer's methods 
+        PlanetModelWrapper _wrapper; 
+
+        // Links to the variables all programmer's share within sharedvariables
+        SharedVariables _variables;
+
+        // Parameters are required to link
+        public JackModel(PlanetModelWrapper wrapper, SharedVariables variables)
         {
-            return false;
+            _wrapper = wrapper;
+            _variables = variables;
         }
-        public bool BuildSchool(int a, int s)
+
+        // Constant strings used in order to build the "essential services" lands
+        private const string BUILD_EMERGENCYSERVICES = "Emergency Services";
+        private const string BUILD_SCHOOL = "School";
+        private const string BUILD_MEDICALFACILITY = "Medical Facility";
+        private const string BUILD_GOVERNMENTFACILITY = "Government Facility";
+        private const string BUILD_POWERPLANT = "Powerplant";
+
+        // Constant strings used in order to build the "industrial facility" lands
+        private const string BUILD_FACTORY = "Factory";
+        private const string BUILD_ENVIRONMENTALFACILITY = "Environmental Facility";
+        
+        // A boolean used to build the Emergency Services using the rows and columns of the grid
+        public bool BuildEmergencyServices(int row, int col)
         {
-            _variables.Facilities[a, s] = new School();
-            return true;
+            return BuildFacility(row, col, BUILD_EMERGENCYSERVICES);
         }
-        public bool BuildMedicalFacility(int a, int s)
+
+        // A boolean used to build the School using the rows and columns of the grid
+        public bool BuildSchool(int row, int col)
         {
-            return false;
+            return BuildFacility(row, col, BUILD_SCHOOL);
         }
-        public bool BuildGovernmentFacility(int a, int s)
+
+        // A boolean used to build the Medical Facility (Hospital) using the rows and columns of the grid
+        public bool BuildMedicalFacility(int row, int col)
         {
-            return false;
+            return BuildFacility(row, col, BUILD_MEDICALFACILITY);
         }
-        public bool BuildPowerPlant(int a, int s)
+
+        // A boolean used to build the Government Facility using the rows and columns of the grid
+        public bool BuildGovernmentFacility(int row, int col)
         {
-            if (_variables.Facilities[a, s] is Dirt)
+            return BuildFacility(row, col, BUILD_GOVERNMENTFACILITY);
+        }
+
+        // A boolean used to build the Powerplant using the rows and columns of the grid
+        public bool BuildPowerPlant(int row, int col)
+        {
+            return BuildFacility(row, col, BUILD_POWERPLANT);
+        }
+
+        // A boolean used to build the Factory using the rows and columns of the grid
+        public bool BuildFactory(int row, int col)
+        {
+            return BuildFacility(row, col, BUILD_FACTORY);
+        }
+
+        // A boolean used to build the Environmental Facility using the rows and columns of the grid
+        public bool BuildEnvironmentalFacility(int row, int col)
+        {
+            return BuildFacility(row, col, BUILD_ENVIRONMENTALFACILITY);
+        }
+
+        /// <summary>
+        /// Checks what the building the user wishes to create is,
+        /// checks the cost and depending on whether or not the user has the required amount of money,
+        /// build the facility or do not do anything
+        /// </summary>
+        /// <param name="row">The row of the grid</param>
+        /// <param name="col">The column of the grid</param>
+        /// <param name="type">The specific building the user wishes to build</param>
+        /// <returns>Returns what action was used (building built or not)</returns>
+        private bool BuildFacility(int row, int col, string type)
+        {
+            // Checks if the type of the building is a Emergency Services Facility
+            if (type == BUILD_EMERGENCYSERVICES)
             {
-                _variables.Facilities[a, s] = new Powerplant();
-                return true;
+                // Checks if the user's money is greater than the cost of Emergency Services
+                if (_variables.Money >= EmergencyServices.COST)
+                {
+                    // Subtract the amount of money the user has with the cost of the Emergency Services
+                    _variables.Money -= EmergencyServices.COST;
+
+                    // Build Emergency Services at the desired grid space
+                    _variables.Facilities[row, col] = new EmergencyServices();
+
+                    // Exits the function because it was built
+                    return true;
+                }
+
+                // Checks if the type of the building is a School
+                else if (type == BUILD_SCHOOL)
+                {
+                    // Checks if the user's money is greater than the cost of a School
+                    if (_variables.Money >= School.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the school
+                        _variables.Money -= School.COST;
+
+                        // Build a school at the desired grid space
+                        _variables.Facilities[row, col] = new School();
+
+                        // Exits the function because it was built
+                        return true;
+                    }
+                }
+
+                // Checks if the type of the building is a Medical Facility
+                else if (type == BUILD_MEDICALFACILITY)
+                {
+                    // Checks if the user's money is greater than the cost of a Medical Facility
+                    if (_variables.Money >= MedicalFacility.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the Medical Facility
+                        _variables.Money -= MedicalFacility.COST;
+
+                        // Build a Medical Facility at the desired grid space
+                        _variables.Facilities[row, col] = new MedicalFacility();
+
+                        // Exits the function because it was built 
+                        return true;
+                    }
+
+                }
+
+                // Checks if the type of the building is a Government Facility
+                else if (type == BUILD_GOVERNMENTFACILITY)
+                {
+                    // Checks if the user's money is greater than the cost of a Government Facility
+                    if (_variables.Money >= GovernmentFacility.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the Government Facility
+                        _variables.Money -= GovernmentFacility.COST;
+
+                        // Build a Government Facility at the desired grid space
+                        _variables.Facilities[row, col] = new GovernmentFacility();
+
+                        // Exits the function because it was built 
+                        return true;
+                    }
+                }
+                // Checks if the type of the building is a Powerplant
+                else if (type == BUILD_POWERPLANT)
+                {
+                    // Checks if the user's money is greater than the cost of a Powerplant
+                    if (_variables.Money >= Powerplant.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the Powerplant
+                        _variables.Money -= Powerplant.COST;
+
+                        // Build a Powerplant at the desired grid space
+                        _variables.Facilities[row, col] = new Powerplant();
+
+                        // Exits the function because it was built
+                        return true;
+                    }
+                }
+                // Checks if the type of the building is a Factory
+                else if (type == BUILD_FACTORY)
+                {
+                    // Checks if the user's money is greater than the cost of a Factory
+                    if (_variables.Money >= Factory.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the Factory
+                        _variables.Money -= Factory.COST;
+
+                        // Build a Factory at the desired grid space
+                        _variables.Facilities[row, col] = new Factory();
+
+                        // Exits the function because it was built
+                        return true;
+                    }
+                }
+                // Checks if the type of the building is an Environmental Facility
+                else if (type == BUILD_ENVIRONMENTALFACILITY)
+                {
+                    // Checks if the user's money is greater than the cost of an Environmental Facility
+                    if (_variables.Money >= EnvironmentalFacility.COST)
+                    {
+                        // Find the difference between the user's current amount of money and the cost of the Environmental Facility
+                        _variables.Money -= EnvironmentalFacility.COST;
+
+                        // Build an Environmental Facility at the desired grid space
+                        _variables.Facilities[row, col] = new EnvironmentalFacility();
+
+                        // Exits the function because it was built
+                        return true;
+                    }
+                }               
             }
+            // Exits the function because nothing could be built
             return false;
         }
-        public bool BuildFactory(int a, int s)
-        {
-            if (_variables.Facilities[a, s] is Dirt)
-            {
-                _variables.Facilities[a, s] = new Factory();
-                return true;
-            }
-            return false;
-        }
-        public bool BuildEnvironmentalFacility(int a, int s)
-        {
-            return false;
-        }
+
+        // Calculates the total pollution of the planet
         public int CalculatePollution()
         {
-            return 5000;
+            // Sets totalPollution as 0 as an int variable
+            int totalPollution = 0;
+
+            // Loop through the rows of the grid
+            for (int i = 0; i < _variables.NumRows; i++)
+            {
+                // Loop through the columns of the grid
+                for (int j = 0; j < _variables.NumCols; j++)
+                {
+                    // Add up the total pollution from all the facilties on the planet and add it all up
+                    totalPollution += _variables.Facilities[i, j].Pollution;
+                }
+            }
+            // Returns the totalPollution when the loop is finished
+            return totalPollution;
         }
-        public int CalculateScore()
+
+        // Calculates the total Score of the planet
+        public long CalculateScore()
         {
-            return 6000;
+            // Sets contentedPopulation as an integer and finds it by subtracting the happypopulation from the population of the planet
+            int contentedPopulation = _wrapper.CalculatePopulation() - _wrapper.CalculateHappyPopulation();
+
+            // Returns the Score by using the given formula of multiplying the happy population by 3, adding it onto contentedpopulation and then subtracting pollution from it 
+            return 3 * _wrapper.CalculateHappyPopulation() + contentedPopulation - _wrapper.CalculatePollution();
         }
-        public void SaveHighScore()
+/*
+        // A method used to save the highscore
+        public void SaveHighScore(string fileName)
         {
+            // Handles any errors that may occur
+            try
+            {
+                    // Creates a file named Highscore.txt if the file is not found
+                    using (StreamWriter file = new StreamWriter("Highscore.txt"))
+                    {
+                        // Writes down the highscore of the player
+                        file.WriteLine(_variables.Score);
+                    } 
+             }
+            // Ends try with catch
+            catch
+            {
+
+            }
         }
-        public void LoadHighScore()
+        
+        // A method used to load the highscore
+        public void LoadHighScore(string fileName)
         {
+            // Handles any errors that may occur
+            try
+            {
+                // Checks if the file "Highscore" exists or not
+                if (File.Exists("Highscore.txt") == true)
+                {
+                    // If the file exists, read the file using StreamReader
+                    using (StreamReader file = new StreamReader("Highscore.txt"))
+                    {   
+                        // Set the text within the file as the Highscore variable
+                        
+                    }
+                }
+                // If the file does not, display a messagebox
+                else
+                {
+                    // Have a messagebox that displays "File could not be found" appear
+                    MessageBox.Show("File could not be found.");
+                }
+            }
+            // Ends try with catch
+            catch
+            {
+
+            }
         }
+*/
     }
 }
+
