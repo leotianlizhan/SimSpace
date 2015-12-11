ec564a5206651213cd6aac840177455f24c2a132
Symbioting <lordterethias@gmail.com>
2015-12-11 09:54:52 -0500

    Trump does not approve.

--- SimSpace_JAT/EnvironmentalFacility.cs
+++ SimSpace_JAT/EnvironmentalFacility.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the EnvironmentalFacility class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class EnvironmentalFacility : Facility
+    class EnvironmentalFacility : IndustrialFacility
     {
+        /// <summary>
+        /// Cost of an Environmental Facility
+        /// </summary>
+        public const int COST = 200000000;
+        
+        /// <summary>
+        /// Creates an EnvironmentalFacility with the assigned attributes
+        /// </summary>
+        public EnvironmentalFacility()
+        {
+            Maintenance = 3000000;
+            Revenue = 0;
+            Pollution = -20000;
+            Power = -75;
+        }
     }
 }
