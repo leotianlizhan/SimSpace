67ab8a3e32bd661c443461a6e825c957167c24f1
Symbioting <lordterethias@gmail.com>
2015-12-11 10:28:58 -0500

    Update IndustrialFacility.cs

--- SimSpace_JAT/IndustrialFacility.cs
+++ SimSpace_JAT/IndustrialFacility.cs
@@ -1,6 +1,6 @@
 // Written by: Jack Wen | Team Members: Andrew, Tianli
 // Date: December 11, 2015
-// Abstract EssentialServicesFacility class that contains all the values shared in all of the child classes
+// Abstract IndustrialFacility class that contains all the values shared in all of the child classes
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +9,9 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    abstract class EssentialServicesFacility : Facility
+    abstract class IndustrialFacility : Facility
     {
-        /// <summary>
-        /// Sets the default revenue and pollution for the child classes
-        /// </summary>
-        public EssentialServicesFacility()
-        {
-            Revenue = 0;
-            Pollution = 0;
-        }
+        // Creates a public constant integer that saves the Range Limit as 6
+        public const int RANGE_LIMIT = 6;
     }
 }
