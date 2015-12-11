602fe5c82942bb3c97791596ef78f52db7432ff5
Symbioting <lordterethias@gmail.com>
2015-12-11 09:58:14 -0500

    Welcome to Springfield.

--- SimSpace_JAT/Powerplant.cs
+++ SimSpace_JAT/Powerplant.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the Powerplant class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class Powerplant : Facility
+    class Powerplant : EssentialServicesFacility
     {
+        /// <summary>
+        /// Cost of a Powerplant
+        /// </summary>
+        public const int COST = 500000000;
+        
+        /// <summary>
+        /// Creates a Powerplant with the given values
+        /// </summary>
+        public Powerplant()
+            : base()
+        {
+            Maintenance = 2000000;
+            Power = 100;
+        }
     }
 }
