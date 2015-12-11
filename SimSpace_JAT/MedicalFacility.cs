e4ea937fdc46b8c532b297073ee4e75a7aed110a
Symbioting <lordterethias@gmail.com>
2015-12-11 09:56:08 -0500

    MEDIC, MEDIC!

--- SimSpace_JAT/MedicalFacility.cs
+++ SimSpace_JAT/MedicalFacility.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the MedicalFacility class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class MedicalFacility : Facility
+    class MedicalFacility : EssentialServicesFacility
     {
+        /// <summary>
+        /// Cost of a Medical Facility
+        /// </summary>
+        public const int COST = 1000000000;
+        
+        /// <summary>
+        /// Creates a Medical Facility with the assigned attributes
+        /// </summary>
+        public MedicalFacility()
+            : base()
+        {
+            Maintenance = 15000000;
+            Power = -20;
+        }
     }
 }
