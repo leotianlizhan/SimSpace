a52c6b2e9239c96d1019533f6ac52c44d7ab46ff
Symbioting <lordterethias@gmail.com>
2015-12-11 09:57:09 -0500

    The building our beloved, Trump resides.

--- SimSpace_JAT/GovernmentFacility.cs
+++ SimSpace_JAT/GovernmentFacility.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the GovernmentFacility class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,21 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class GovernmentFacility : Facility
+    class GovernmentFacility : EssentialServicesFacility
     {
+        /// <summary>
+        /// Cost of a Government Facility
+        /// </summary>
+        public const int COST = 10000000;
+
+        /// <summary>
+        /// Creates a Government Facility with the assigned values
+        /// </summary>
+        public GovernmentFacility()
+            : base()
+        {
+            Maintenance = 1000000;
+            Power = -10; 
+        }
     }
 }
