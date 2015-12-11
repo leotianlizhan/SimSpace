20886156604a7f1d0d99b657419861767a1fcf08
Symbioting <lordterethias@gmail.com>
2015-12-11 09:53:37 -0500

    Let's get minimum wage, together! :D

--- SimSpace_JAT/Factory.cs
+++ SimSpace_JAT/Factory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// This is the Factory class that creates a factory with the given values
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class Factory : Facility
+    class Factory : IndustrialFacility
     {
+        /// <summary>
+        /// Cost of Factory
+        /// </summary>
+        public const int COST = 50000000;
+
+        /// <summary>
+        /// Creates a factory with the given attributes
+        /// </summary>
+        public Factory()
+        {
+            Maintenance = 500000;
+            Revenue = 5000000;
+            Pollution = 20000;
+            Power = -50;
+        }
     }
 }
