be2b49c445d64269195ad2259231506f4aff9651
Symbioting <lordterethias@gmail.com>
2015-12-11 09:59:58 -0500

    This is ESSENTIAL, ESSENTIAL!

--- SimSpace_JAT/EssentialServicesFacility.cs
+++ SimSpace_JAT/EssentialServicesFacility.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Written by: Jack Wen | Team Members: Andrew, Tianli
+// Date: December 11, 2015
+// Abstract IndustrialFacility class that contains all the values shared in all of the child classes
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,9 @@ using System.Threading.Tasks;
 
 namespace SimSpace_JAT
 {
-    class EssentialServicesFacility : Facility
+    abstract class IndustrialFacility : Facility
     {
+        // Creates a public constant integer that saves the Range Limit as 6
+        public const int RANGE_LIMIT = 6;
     }
 }
